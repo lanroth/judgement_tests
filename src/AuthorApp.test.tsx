4bc8a3c8b7de477712e7f41a828a8a1586c89148
Martin <martin.mphil@gmail.com>
2020-03-06 10:10:30 +0000

    transferred Assessor and Author to new repo

--- src/AuthorApp.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./AuthorApp";
-import ReactDOM from "react-dom";
-
-test("App renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
