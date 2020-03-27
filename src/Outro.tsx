fd328860619efdb09e49b52a6a2d3070318ad80e
Martin <martin.mphil@gmail.com>
2020-03-27 09:08:21 +0000

    minor change to outro text formatting

--- src/Outro.tsx
+++ src/Outro.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Outro: React.FC = () => {
   return (
-    <p>
-      Congratulations, you completed your test. We will be in touch soon about
-      your results and let you know the next steps. Thank you and goodbye.
-    </p>
+    <main>
+      <p>Congratulations, you completed your test.</p>
+      <p>
+        We will be in touch soon about your results and let you know the next
+        steps.
+      </p>
+      <p>Thank you and goodbye.</p>
+    </main>
   );
 };
 
