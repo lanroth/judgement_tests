207b36cc74e2c3daf11a1acffe8e36cf10cdad52
Martin <martin.mphil@gmail.com>
2020-03-28 13:17:19 +0000

    config backend for dev and production URLs

--- src/ConfigSjt.tsx
+++ src/ConfigSjt.tsx
@@ -1,1 +1,6 @@
-export const backend = "https://lanroth.com/sjt-backend-sb/";
+export const backend =
+  window.location.hostname === "sjt.workbrighter.co.uk"
+    ? "https://sjt-api.workbrighter.co.uk/sjt-backend-sb/"
+    : "https://lanroth.com/sjt-backend-sb/";
+
+// export const backend = "https://lanroth.com/sjt-backend-sb/";
